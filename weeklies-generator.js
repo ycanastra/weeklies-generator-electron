4364db44843c3acd6d0864a71b1621f58032a6bc
Yushi Canastra <Yushi Canastra>
2016-09-04 15:47:21 -0700

    creates titles now

--- weeklies-generator.js
+++ weeklies-generator.js
@@ -3,18 +3,40 @@ const exceljs = require('exceljs')
 const numDays = 7;
 const maxCol = numDays;
 
+const styles = {
+	title: {
+		font : {
+			name: 'Verdanana',
+			size: 24
+		},
+		alignment: {
+			vertical: 'middle',
+			horizontal: 'center'
+		}
+	}
+}
+
 class WeekliesGenerator {
   constructor(labNames, week) {
 		this.labNames = labNames;
 		this.week = week;
 		this.workbook = new exceljs.Workbook();
   }
+	
 	generateWeeklies() {
 		var workbook = new exceljs.Workbook();
 
-		this.labNames.forEach(labName => this.createWorksheet(labName));
+		this.labNames.forEach((labName) => {
+			this.createWorksheet(labName);
+		});
+
+		this.workbook.eachSheet((ws, sheetId) => {
+			this.createTitle(ws, this.labNames[sheetId - 1]);
+		})
+
 		this.saveWorkbook('test.xlsx');
 	}
+
 	createWorksheet(labName) {
 		this.workbook.addWorksheet(labName);
 	}
@@ -24,6 +46,12 @@ class WeekliesGenerator {
     .then(function() {
         // done
     });
+	}
+	createTitle(ws, titleName) {
+		ws.mergeCells(1, 1, 1, 7);
+		ws.getCell(1, 1, 1, 1).value = titleName;
+		ws.getCell(1, 1, 1, 1).font = styles.title.font;
+		ws.getCell(1, 1, 1, 1).alignment = styles.title.alignment;
 	}
 }
 
